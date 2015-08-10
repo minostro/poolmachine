647188f78393b23e3b358d8d9baabe9bd97bfaa5
Milton Inostroza <minostro@minostro.com>
2015-08-10 10:19:43 -0500

    it does not register process for pool creation

--- src/pool/poolmachine_pool.erl
+++ src/pool/poolmachine_pool.erl
@@ -3,7 +3,7 @@
 -behaviour(supervisor).
 
 %% API
--export([start_link/2]).
+-export([start_link/1, start_worker_sup/1]).
 
 %% Supervisor callbacks
 -export([init/1]).
@@ -14,33 +14,34 @@
 %% API functions
 %%====================================================================
 
-start_link(Name, Properties) ->
-  supervisor:start_link({local, pool_name(Name)}, ?MODULE, [Name, Properties]).
+start_link(Properties) ->
+  supervisor:start_link(?MODULE, [Properties]).
+
+start_worker_sup(Pid) ->
+  WorkerSupSpec = {
+    poolmachine_pool_worker_sup,
+    {poolmachine_pool_worker_sup, start_link, []},
+    temporary,
+    brutal_kill,
+    supervisor,
+    []
+  },
+  supervisor:start_child(Pid, WorkerSupSpec).
+
 
 %%====================================================================
 %% Supervisor callbacks
 %%====================================================================
 
 %% Child :: {Id,StartFunc,Restart,Shutdown,Type,Modules}
-init([Name, Properties]) ->
+init([Properties]) ->
   SupFlags = {one_for_one,1, 5},
   PoolManagerSpec = {
     poolmachine_pool_manager,
-    {poolmachine_pool_manager, start_link, [Name, Properties]},
-    temporary,
+    {poolmachine_pool_manager, start_link, [self(), Properties]},
+    transient,
     brutal_kill,
     worker,
     []
   },
-  WorkerSupSpec = {
-    poolmachine_pool_worker_sup,
-    {poolmachine_pool_worker_sup, start_link, [Name]},
-    temporary,
-    brutal_kill,
-    worker,
-    []
-  },
-  {ok, {SupFlags, [PoolManagerSpec, WorkerSupSpec]}}.
-
-pool_name(Name) ->
-  list_to_atom(Name ++ "_pool").
+  {ok, {SupFlags, [PoolManagerSpec]}}.
