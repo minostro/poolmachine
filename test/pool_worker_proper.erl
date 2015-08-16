840649a7490b6d1dd33cd4d12b453047ff5ec392
Milton Inostroza <minostro@minostro.com>
2015-08-15 22:22:20 -0500

    fixes failing test

--- test/pool_worker_proper.erl
+++ test/pool_worker_proper.erl
@@ -18,7 +18,7 @@ prop_run_good_task() ->
 
       poolmachine_pool_worker:stop(Pid, normal),
       %%Please check tasks/good_task.erl, line 7
-      TestResult =:=  [Args] ++ ["test2"]
+      TestResult =:=  [Args] ++ ["test"]
     end).
 
 prop_run_bad_task() ->
